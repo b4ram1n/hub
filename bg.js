d0a2dec4aa229afd1a32db63547cc23b2027c2df
b4ram1n <k4k5k1k2k3@gmail.com>
2025-10-12 22:27:31 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -22,7 +22,27 @@ particlesJS('particles-js', {
 			"onhover": { "enable": true, "mode": "repulse" },
 			"onclick": { "enable": true, "mode": "push" }
 		},
-		"modes": { "repulse": { "distance": 200, "duration": 0.4 } }
+		"modes": {
+			"repulse": { "distance": 200, "duration": 0.4 },
+			"push": { "particles_nb": 3 }
+		}
 	},
 	"retina_detect": true
 });
+
+setTimeout(() => {
+  console.log('particles.js loaded');
+
+  const maxParticles = 100;// 最大数
+  const checkInterval = 500;// ms
+
+  const pJSDom = window.pJSDom && window.pJSDom[0];
+  if (!pJSDom) return;
+
+  setInterval(() => {
+    const particles = pJSDom.pJS.particles.array;
+    if (particles.length > maxParticles) {
+      particles.splice(0, particles.length - maxParticles);
+    }
+  }, checkInterval);
+}, 500);
