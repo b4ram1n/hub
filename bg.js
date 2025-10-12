56bcba8d8c5e2316c19504dac9fc60b58b8e7543
b4ram1n <k4k5k1k2k3@gmail.com>
2025-10-12 22:09:59 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,28 @@
+particlesJS('particles-js', {
+	"particles": {
+		"number": {
+			"value": 8,
+			"density": { "enable": true, "value_area": 800 }
+		},
+		"color": { "value": "#ffffff" },
+		"shape": { "type": "circle" },
+		"opacity": { "value": 0.5 },
+		"size": { "value": 3, "random": true },
+		"line_linked": {
+			"enable": true,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": { "enable": true, "speed": 6 }
+	},
+	"interactivity": {
+		"events": {
+			"onhover": { "enable": true, "mode": "repulse" },
+			"onclick": { "enable": true, "mode": "push" }
+		},
+		"modes": { "repulse": { "distance": 200, "duration": 0.4 } }
+	},
+	"retina_detect": true
+});
